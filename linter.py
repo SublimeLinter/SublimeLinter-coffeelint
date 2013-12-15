0af198c62cc5f8dd5885b934f60e66ef6d7e609d
Aparajita Fishman <aparajita@aparajita.com>
2013-12-14 22:10:20 -0800

    Initial commit.

---
+++ linter.py
@@ -0,0 +1,38 @@
+#
+# coffeelint.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Aparajita
+# Copyright (c) 2013 Aparajita
+#
+# License: MIT
+#
+
+"""This module exports the Coffeelint plugin class."""
+
+from SublimeLinter.lint import Linter, persist
+
+
+class Coffeelint(Linter):
+
+    """Provides an interface to coffeelint."""
+
+    syntax = ('coffeescript', 'coffeescript_literate')
+    executable = 'coffeelint'
+    regex = (
+        r'^<issue line="(?P<line>\d+)"\s*\r?\n'
+        r'\s*lineEnd="\d+"\s*\r?\n'
+        r'\s*reason="\[(?:(?P<error>error)|(?P<warning>warn))\] (?P<message>.+?)"\s*\r?\n'
+    )
+    multiline = True
+    comment_re = r'\s*#'
+
+    def cmd(self):
+        """Return a tuple with the command line to execute."""
+
+        result = ['coffeelint', '--jslint', '--stdin']
+
+        if persist.get_syntax(self.view) == 'coffeescript_literate':
+            result.append('--literate')
+
+        return result
